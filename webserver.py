37f25fa41e6a995c0a568696db72e1ad55e6b222
KJQUIN17 <KJQUIN17@g.holycross.edu>
2017-02-07 23:20:23 -0500

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -16,9 +16,10 @@ def gen_random():
     return random.randint(1, 100)  # Integer from 1 to 100, endpoints included
 
 
-def http_response():  # fix static vs dynamic vs page not found
+def http_response(st_in):  # fix static vs dynamic vs page not found
     time_stamp = timestamp()
     rand = gen_random()
+    print"Data Captured: ", st_in
 
     header = '''HTTP/1.1 200 OK
 Date: %s
@@ -27,23 +28,50 @@ Last-Modified: Sun, 14 Feb 2010 21:57:18 GMT (fix)
 Content-Length: 57 (fix)
 Connection: close
 Content-Type: text/plain
+Edit - The time is now %s. Bye!''' % (time_stamp, rand, time_stamp)
 
-The time is now %s. Bye!'''
-    return header % (time_stamp, rand, time_stamp)
+    return header
+
+
+def check_content_type(header):
+    pattern = re.compile("hello (.*?)\n")
+    content_type = pattern.search(str(header))  # Dynamic Keyword Search: "hello"
+    return content_type
+
+
+def dynamic_response():
+    rand = gen_random()
+    response = "Generic Dyanmic Response - %d\n" % rand
+    return response
+
+
+def file_check(directory, fileA):
+    filename = str(directory) + str(fileA)
+    print filename
+    try:
+        open(filename, "r")
+    except IOError:
+        print "Error: File Does Not Exist"
+    else:
+        return filename
 
 
 def parse_request(header):
+    split_header = []
     pattern = re.compile("GET (.*?)\n")
     parse_header = pattern.match(header)  # return a corresponding MatchObject. Return None if the string != pattern
 
-    # TODO: Potentially use re.split(pattern, string, maxsplit=0, flags=0) to return string
-    return parse_header
+    if parse_header is not None:
+        split_header = re.split("\s+", header)
+
+    return parse_header, split_header
 
 
 #########################################
 # Kyle Quinn
 # CSCI 356 Networks
 # Project 1: Checkpoint 9pm Tuesday 2/14
+# Last Modified: 2/7/17 11:19pm
 #########################################
 
 if __name__ == "__main__":
@@ -61,6 +89,7 @@ if __name__ == "__main__":
     #  SOCK_STREAM constants represent the socket types
     s.bind((HOST, PORT))
     s.listen(1)  # Listen for connections made to the socket. The backlog argument specifies MAX# queued connections
+    print"Listening on PORT %d\n" % PORT  # Error Checking
     conn, addr = s.accept()  # Accept a connection. The socket must be bound to an address and listening for connections
     print 'Connected by', addr
     while 1:
@@ -69,22 +98,26 @@ if __name__ == "__main__":
         if not data:
             break
         else:
-            request = parse_request(data)  # Read from that new socket, parse the HTTP request.
-            if request is not None:
-                # Request is a GET request
-                send = http_response()
-                conn.sendall(send)  # Send data to the socket. The socket must be connected to a remote socket. \
+            request, fn = parse_request(data)  # Read from that new socket, parse the HTTP request.
+            print("Parsing Request.\n")
+            if request:
+                dynamic_content = check_content_type(request)
+                if dynamic_content:
+                    send = dynamic_response()
+                    conn.sendall(send)  # Send data to the socket. The socket must be connected to a remote socket.
+                    print"Sent response, content length was %d\n" % (len(send))
+                else:
+                    parsed_file = file_check(DIR, fn[1])
+                    if parsed_file:
+                        send = 'Sent File ... Placeholder'
+                        conn.sendall(send)  # Send data to the socket. The socket must be connected to a remote socket.
+                        print"Sent response, content length was %d\n" % (len(send))
+                    else:
+                        print("Error 404! Invalid? \n")
             else:
-                print("Static case, just print an error message and quit!\n")
-                fn_path = str(DIR) + str(send)  # TODO  - be able to open this file in firefox
-                print fn_path
-
-#                 Fix case 2.d above by adding code to handle static content:
-# Convert the request path into a file path, by appending it to the document root from the command line.
-# Determine if that file exists. Send an HTTP/1.1 404 response if it does not.
-# Open and read the contents of that file, then send the contents back inside an HTTP/1.1 200 OK response.
-# Close the connection, as before.
+                print("This request is Not a GET Request!\n")
+                # TODO Extra Credit Goes Here!
                 break
-        # this method continues to send data from string until either all data has been sent or an error occurs. \
-        # None is returned on success
     conn.close()
+    print"Connection Closed.\n"
+
